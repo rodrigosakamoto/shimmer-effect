394a840d17fe2b3a49e4e80e4552bacd4236f9f9
rodrigosakamoto <rodosakamoto@gmail.com>
2020-08-23 10:54:51 -0300

    feat: add shimmer effect

--- src/components/MiddleColumn/index.tsx
+++ src/components/MiddleColumn/index.tsx
@@ -3,16 +3,31 @@ import React from 'react';
 import FeedShare from './FeedShare';
 import FeedPost from './FeedPost';
 
+import LoadingFeedShare from '../Shimmer/LoadingFeedShare';
+import LoadingFeedPost from '../Shimmer/LoadingFeedPost';
+
 import { Container } from './styles';
 
-const MiddleColumn: React.FC = () => {
+const MiddleColumn: React.FC<LoadingProps> = ({isLoading}) => {
   return (
     <Container className="middle-column">
-      <FeedShare />
-      <FeedPost />
-      <FeedPost />
-      <FeedPost />
-      <FeedPost />
+      {isLoading ? (
+        <>
+          <LoadingFeedShare />
+          <LoadingFeedPost />
+          <LoadingFeedPost />
+          <LoadingFeedPost />
+          <LoadingFeedPost />
+        </>
+      ): (
+        <>
+        <FeedShare />
+        <FeedPost />
+        <FeedPost />
+        <FeedPost />
+        <FeedPost />
+        </>
+      )}
     </Container>
   );
 };
